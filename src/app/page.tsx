a6890014317750b5d19eabc1b51e73bb68c874f1
yarocruz <yarocruz@gmail.com>
2024-03-11 20:35:59 -0400

    add background color gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Welcome to my portfolio</h1>
+      <h1 className="text-6xl font-bold">JAY CRUZ</h1>
       <p className="text-2xl">
         This is my portfolio. There are many like it, but this one is mine.
       </p>      
