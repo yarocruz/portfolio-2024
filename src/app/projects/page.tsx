02a77ac003ac1f5c26cfb4efcf1476d7c84d9a76
yarocruz <yarocruz@gmail.com>
2024-03-14 20:39:50 -0400

    setup projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Projects() {
+  return (
+    <div className="flex flex-col">
+      <nav className="mb-5">
+        <ul className="flex font-bold text-xl">
+          <li className="mr-5">
+            <Link href="/">ABOUT</Link>
+          </li>
+          <li className="underline underline-offset-8 decoration-sky-500">
+            <Link href="/projects">PROJECTS</Link>
+          </li>
+        </ul>
+      </nav>
+      <section className="section">
+        <h1>TODOS</h1>
+      </section>
+    </div>
+  );
+}
