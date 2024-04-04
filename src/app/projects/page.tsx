fd3a55c55d84c53171bf260fa97fc2425a2ebdfb
yarocruz <yarocruz@gmail.com>
2024-04-04 15:01:44 -0400

    add "real" projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,49 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-
-const PROJECTS = [  
-    {
-        id: 1,
-        title: "podlr",
-        short_description: "Letterboxd for podcasts",
-        long_description: "Podlr is a web application that allows users to keep track of the podcasts they listen to. Users can rate, review, and keep track of their favorite episodes. podlr also provides a social aspect, allowing users to follow their friends and see what they are listening to.",
-        demo: "https://podlr.vercel.app/",
-        github: "https//github.com/jaycruz/podlr"
-    },
-    {
-        id: 2,
-        title: "anchor",
-        short_description: "Link bookmarking application",
-        long_description: "Anchor is a web application that allows users to keep track of the links they find on the internet. Users can save, categorize, and keep track of their favorite links. anchor also provides a social aspect, allowing users to follow their friends and see what they are saving.",
-        demo: "https://anchor.vercel.app/",
-        github: "https//github.com/jaycruz/anchor"
-    },
-    {
-        id: 3,
-        title: "garden",
-        short_description: "Gardening application",
-        long_description: "Garden is a web application that allows users to keep track of the plants they have in their garden. Users can rate, review, and keep track of their favorite plants. garden also provides a social aspect, allowing users to follow their friends and see what they are growing.",
-        demo: "https://garden.vercel.app/",
-        github: "https//github.com/jaycruz/garden"
-    },
-    {
-        id: 4,
-        title: "taste",
-        short_description: "Recipe application",
-        long_description: "Taste is a web application that allows users to keep track of the recipes they find on the internet. Users can save, categorize, and keep track of their favorite recipes. taste also provides a social aspect, allowing users to follow their friends and see what they are cooking.",
-        demo: "https://taste.vercel.app/",
-        github: "https//github.com/jaycruz/taste"
-    },
-    {
-        id: 5,
-        title: "journal",
-        short_description: "Journaling application",
-        long_description: "Journal is a web application that allows users to keep track of their thoughts. Users can write, categorize, and keep track of their journal entries. journal also provides a social aspect, allowing users to follow their friends and see what they are writing.",
-        demo: "https://journal.vercel.app/",
-        github: "https//github.com/jaycruz/journal"
-    }
-];
+import { PROJECTS } from "./projects";
 
 export default function Projects() {
   const [selectedProjectID, setSelectedProjectID] = useState(1)
@@ -52,7 +10,7 @@ export default function Projects() {
     setSelectedProjectID(id)
   }
 
-  const filteredProject = PROJECTS.filter(project => project.id === selectedProjectID)
+  const project = PROJECTS.filter(project => project.id === selectedProjectID)
 
   return (
     <div className="flex flex-col my-10">
@@ -64,6 +22,7 @@ export default function Projects() {
           <li className=" underline underline-offset-8 decoration-sky-500">
             <Link href="/projects">PROJECTS</Link>
           </li>
+          {/* ADD Section to download resume */}
         </ul>
       </nav>
       <section className="section lg:h-80">
@@ -80,11 +39,12 @@ export default function Projects() {
                 ))}
             </ul>
             <section className="flex flex-col">
-                <h1 className="logo text-lg font-black mb-5">{`${filteredProject[0].title.toUpperCase()} - ${filteredProject[0].short_description}`}</h1>
-                <p className="mb-5 lg:w-10/12">{filteredProject[0].long_description}</p>
+                <h1 className="logo text-lg font-black mb-5">{`${project[0].title.toUpperCase()} - ${project[0].short_description}`}</h1>
+                <p className="mb-5 lg:w-10/12">{project[0].long_description}</p>
                 <div className="flex text-cyan-400">
-                  <span className="mr-4 transition duration-300 hover:scale-110"><a href="#">demo</a></span>
-                  <span className="transition duration-300 hover:scale-110"><a href="#">code</a></span>
+                  {/* TODO When hovering over demo link, show a preview image of project */}
+                  <span className="mr-4 transition duration-300 hover:scale-110"><a href={project[0].demo} target="_blank">demo</a></span>
+                  <span className="transition duration-300 hover:scale-110"><a href={project[0].github} target="_blank">code</a></span>
                 </div>
             </section>
         </div>
---
+++ src/app/projects/projects.ts
@@ -0,0 +1,26 @@
+export const PROJECTS = [  
+    {
+        id: 1,
+        title: "rpsls",
+        short_description: "Rock Paper Scissors Lizard Spock",
+        long_description: "Rock Paper Scissors Lizard Spock is an expansion of the game Rock, Paper, Scissors. It was created by Sam Kass and Karen Bryla, but it was popularized in an episode of the Big Bang Theory called The Lizard-Spock expansion. I built it to learn more about the Vue.js library and to learn the basics of game logic.",
+        demo: "https://yarocruz.github.io/rpsls/",
+        github: "https://github.com/yarocruz/rpsls"
+    },
+    {
+        id: 2,
+        title: "cpg",
+        short_description: "Color palette generator",
+        long_description: "Color palette generator uses OpenAI's Completion API to generate color palettes based on a given prompt. Depending on the prompt it can generate 2 to 5 colors. The colors will be displayed on the page and the hex codes can be copied to your clipboard. Node/Express was used for backend, and plain HTML, CSS, and JavaScript for frontend.",
+        demo: "https://color-palette-generator.herokuapp.com/",
+        github: "https://github.com/yarocruz/color-palette-generator/tree/main"
+    },
+    {
+        id: 3,
+        title: "RSA",
+        short_description: "Reading Speed App",
+        long_description: "Single-serving app to see what it feels like to read at different speeds. It's a tiny app I quickly built with Svelte to scratch my own itch. According to the research, the average reading speed is around 250 words per minute. I did the math and that's about 4.5 words a minute, which is 1 word every 250 milliseconds. That last piece of math I'm not so sure.",
+        demo: "https://yarocruz.github.io/reading-speed-svelte-app/",
+        github: "https://github.com/yarocruz/reading-speed-svelte-app"
+    },
+];
