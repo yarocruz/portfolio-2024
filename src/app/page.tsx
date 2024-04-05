f66c1ae8d97d06ce7c48eb7255338af886caa0be
yarocruz <yarocruz@gmail.com>
2024-04-05 12:22:15 -0400

    change wheelhouse because Rob doesnt like it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
           className="sm:float-right rounded mb-5 sm:mb-1 md:mb-5 sm:ml-5 w-auto sm:w-1/2 md:w-1/3 lg:w-1/4" 
         />
         <h1 className="text-2xl font-black mb-3">Hello 👋 I&apos;m Jay Cruz</h1>
-        <p>I’m a Software Engineer and my wheelhouse has been in
-        Web Development. My specialty has been in crafting user interfaces using React/Next.js and 
+        <p>I’m a Software Engineer and my jam has been Web Development. 
+        My specialty has been in crafting user interfaces using React/Next.js and 
         developing robust RESTful APIs with Node/Express with SQL databases. Beyond Web Development, 
         I&apos;ve been broadening my horizon by diving into languages like Python, Rust, and Golang, 
         aiming to tackle unique software challenges across various aspects of technology.</p>
