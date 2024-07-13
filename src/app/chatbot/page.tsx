3af3c11d0a3c74023b0b7dbeda3de5f7a213e4ff
yarocruz <yarocruz@gmail.com>
2024-07-13 18:57:32 -0400

    add chatbot page and raw ui and logic

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,6 +1,23 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+    const [question, setQuestion] = useState('')    
+    const [answer, setAnswer] = useState('')
+  
+    const fetchData = async () => {
+        const response = await fetch('/api', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ text: question })
+        });
+        const data = await response.json();
+        setAnswer(data.result);
+      };
+
   return (
     <div className="flex flex-col my-10">
       <nav className="mb-5">
@@ -17,7 +34,23 @@ export default function Home() {
         </ul>
       </nav>
       <section className="section lg:h-80 overflow-auto leading-relaxed">
-       THIS IS THE CHATBOT PAGE 
+        <h1 className="text-2xl font-black mb-3">Ask my Bot</h1>
+        <p className="my-3">My chatbot is a really simple bot built Open AI API and Langchain. For now 
+            it can only answer questions about me based on my resume, 
+            but I plan to expand it to answer more questions in the future.
+        </p>
+        <input 
+          type="text"
+          placeholder="Ask away..." 
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          className="border-2 border-gray-300 rounded-lg p-2 w-full mb-5 text-black"
+        />
+        <button 
+          onClick={fetchData}
+          className="bg-cyan-400 text-white font-bold py-2 px-4 rounded"
+        >Submit</button>
+       <p className="my-3">{answer}</p>
       </section>
     </div>
   );
